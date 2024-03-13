87abf8242bb263ef1bcefde1acc25d05e1d2292d
DIPAK JAWKAR <120739704+Dipakjawkar@users.noreply.github.com>
2024-03-14 01:41:10 +0530

    Update index.js

--- files_uploading_website_api/index.js
+++ files_uploading_website_api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 
 app.use('/api/v1/user',require('./routes/userRoutes'))
